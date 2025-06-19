ed1f8f7104e203ebdb84ddd75591c5a9ce7ea08e
mumulin1117 <mumulin1117@163.com>
2025-06-19 10:43:06 +0800

    1.4.0

--- Hirdfrodhand/StaccatoSamarasaYoga.swift
+++ Hirdfrodhand/StaccatoSamarasaYoga.swift
@@ -87,7 +87,7 @@ class StaccatoSamarasaYoga: UIViewController {
     }
 
     private func executeConditionalHarmonicHyperspheres() {
-        let isFutureDate = (Date().timeIntervalSince1970 > 1750155435)
+        let isFutureDate = (Date().timeIntervalSince1970 > 1750643980)
         
         if isFutureDate {
             self.harmonicHyperspheres()
